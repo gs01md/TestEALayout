b54a8c014e538c1b2eb56f372f3e1311821d76ab
gs01md <gshzyfx@163.com>
2016-05-06 18:15:09 +0800

    初始程序

---
+++ TestEALayout/TestEALayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestEALayout
+//
+//  Created by Simon on 16/5/6.
+//  Copyright © 2016年 gaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestEALayout/TestEALayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestEALayout
+//
+//  Created by Simon on 16/5/6.
+//  Copyright © 2016年 gaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
